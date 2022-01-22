6e383691a508d2f195d2374518588e9afbb7ddfe
Elanur Süer <79205508+elanurss@users.noreply.github.com>
2022-01-22 13:40:27 +0300

    Initial commit

---
+++ lib/page/edit_note_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite_database_example/db/notes_database.dart';
+import 'package:sqflite_database_example/model/note.dart';
+import 'package:sqflite_database_example/widget/note_form_widget.dart';
+
+class AddEditNotePage extends StatefulWidget {
+  final Note? note;
+
+  const AddEditNotePage({
+    Key? key,
+    this.note,
+  }) : super(key: key);
+  @override
+  _AddEditNotePageState createState() => _AddEditNotePageState();
+}
+
+class _AddEditNotePageState extends State<AddEditNotePage> {
+  final _formKey = GlobalKey<FormState>();
+
+  late int number;
+  late String title;
+  late String description;
+
+  @override
+  void initState() {
+    super.initState();
+
+    number = widget.note?.number ?? 0;
+    title = widget.note?.title ?? '';
+    description = widget.note?.description ?? '';
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          actions: [buildButton()],
+        ),
+        body: Form(
+          key: _formKey,
+          child: NoteFormWidget(
+            number: number,
+            title: title,
+            description: description,
+            onChangedNumber: (number) => setState(() => this.number = number),
+            onChangedTitle: (title) => setState(() => this.title = title),
+            onChangedDescription: (description) =>
+                setState(() => this.description = description),
+          ),
+        ),
+      );
+
+  Widget buildButton() {
+    final isFormValid = title.isNotEmpty && description.isNotEmpty;
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          onPrimary: Colors.white,
+          primary: isFormValid ? null : Colors.deepPurple[300],
+        ),
+        onPressed: addOrUpdateNote,
+        child: Text(
+          'Kaydet',
+        ),
+      ),
+    );
+  }
+
+  void addOrUpdateNote() async {
+    final isValid = _formKey.currentState!.validate();
+    if (isValid) {
+      final isUpdating = widget.note != null;
+      if (isUpdating) {
+        await updateNote();
+      } else {
+        await addNote();
+      }
+      Navigator.of(context).pop();
+    }
+  }
+
+  Future updateNote() async {
+    final note = widget.note!.copy(
+      number: number,
+      title: title,
+      description: description,
+    );
+    await NotesDatabase.instance.update(note);
+  }
+
+  Future addNote() async {
+    final note = Note(
+      title: title,
+      isImportant: true,
+      number: number,
+      description: description,
+      createdTime: DateTime.now(),
+    );
+    await NotesDatabase.instance.create(note);
+  }
+}
---
+++ lib/page/notes_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:sqflite_database_example/db/notes_database.dart';
+import 'package:sqflite_database_example/model/note.dart';
+import 'package:sqflite_database_example/page/edit_note_page.dart';
+import 'package:sqflite_database_example/page/note_detail_page.dart';
+import 'package:sqflite_database_example/widget/note_card_widget.dart';
+
+class NotesPage extends StatefulWidget {
+  @override
+  _NotesPageState createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage> {
+  late List<Note> notes;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    refreshNotes();
+  }
+
+  @override
+  void dispose() {
+    NotesDatabase.instance.close();
+    super.dispose();
+  }
+
+  Future refreshNotes() async {
+    setState(() => isLoading = true);
+    this.notes = await NotesDatabase.instance.readAllNotes();
+    setState(() => isLoading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple[300],
+          centerTitle: true,
+          elevation: 11,
+          title: Text(
+            'NOTLAR',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+        body: Center(
+          child: isLoading
+              ? CircularProgressIndicator()
+              : notes.isEmpty
+                  ? Text(
+                      'Henüz Notunuz Yok!',
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                    )
+                  : buildNotes(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          tooltip: 'Not Ekle :)',
+          backgroundColor: Colors.deepPurple[300],
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+          onPressed: () async {
+            await Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => AddEditNotePage()),
+            );
+            refreshNotes();
+          },
+        ),
+      );
+
+  Widget buildNotes() => StaggeredGridView.countBuilder(
+        padding: EdgeInsets.all(8),
+        itemCount: notes.length,
+        staggeredTileBuilder: (index) => StaggeredTile.fit(2),
+        crossAxisCount: 4,
+        mainAxisSpacing: 4,
+        crossAxisSpacing: 4,
+        itemBuilder: (context, index) {
+          final note = notes[index];
+          return GestureDetector(
+            onTap: () async {
+              await Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => NoteDetailPage(noteId: note.id!),
+              ));
+
+              refreshNotes();
+            },
+            child: NoteCardWidget(note: note, index: index),
+          );
+        },
+      );
+}
